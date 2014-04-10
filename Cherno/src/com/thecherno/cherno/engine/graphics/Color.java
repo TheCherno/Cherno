b786ec2bfbf1b075ad0b9101dfdfc9d074b8ae2d
lost-RD <lost_RD@live.com>
2014-04-10 14:41:52 +0930

    Update Color.java
    added YELLOW

--- Cherno/src/com/thecherno/cherno/engine/graphics/Color.java
+++ Cherno/src/com/thecherno/cherno/engine/graphics/Color.java
@@ -5,6 +5,7 @@ public class Color {
 	public static final Color WHITE = new Color(0xffffff);
 	public static final Color BLACK = new Color(0x0);
 	public static final Color ORANGE = new Color(0xffb300);
+	public static final Color YELLOW = new Color(0xffff00);
 	public static final Color RED = new Color(0xff0000);
 	public static final Color GREEN = new Color(0xff00);
 	public static final Color BLUE = new Color(0xff);

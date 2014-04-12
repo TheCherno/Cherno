da9e1306a9267e6b64874f34f1167752a2382c4d
InzaneNova <InzaneNova@users.noreply.github.com>
2014-04-12 14:10:18 +0200

    Update Sprite.java
    added a constructor just taking in a string for the file name, no need to know about textures.

--- Cherno/src/com/thecherno/cherno/engine/graphics/Sprite.java
+++ Cherno/src/com/thecherno/cherno/engine/graphics/Sprite.java
@@ -12,6 +12,10 @@ public class Sprite implements Renderable {
 		height = texture.getHeight();
 		pixels = texture.getPixels();
 	}
+	
+	public Sprite(String fileName) {
+		Sprite(Texture.load(fileName);
+	}
 
 	public int getWidth() {
 		return width;
